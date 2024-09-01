5a45226568b15758ea312b3615b3d667e33ad164
upperc <daniverschoor@outlook.com>
2024-09-01 11:30:19 +0200

    nl translation

--- src/routes/nl/+page.svelte
+++ src/routes/nl/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-  import ProjectCard from "./ProjectCard.svelte";
-  import Dami from "./Dami.svelte";
-  import Subtitle from "./Subtitle.svelte";
-  import IconLink from "./IconLink.svelte";
-  import SiteObject from "./SiteObject.svelte";
+  import ProjectCard from "../ProjectCard.svelte";
+  import Dami from "../Dami.svelte";
+  import Subtitle from "../Subtitle.svelte";
+  import IconLink from "../IconLink.svelte";
+  import SiteObject from "../SiteObject.svelte";
 </script>
 
 <svelte:head>
@@ -16,37 +16,37 @@
 <main>
 <SiteObject>
   <h1>Dani Verschoor</h1>
-  <h3>Creative Developer</h3>
-  <p class="tagline">Building stable and accessible software.</p>
+  <h3>Creatief Ontwikkelaar</h3>
+  <p class="tagline">Stabiele en toegankelijke software aan het bouwen.</p>
 </SiteObject>
 <div class="project-wrapper">
 <SiteObject>
-  <Subtitle>Web Development</Subtitle>
+  <Subtitle>Web Ontwikkeling</Subtitle>
 </SiteObject>
 <div class="section">
-  <ProjectCard image="claire.png" alt="Wooden case containing electronics. Text saying Claire with air highlighted.">
+  <ProjectCard image="claire.png" alt="Houten omhulsel met electronica. Tekst erop: Claire. Het woord air is gemarkeerd.">
     <h2>Claire</h2>
     <IconLink href="https://github.com/unlimited-hr/Claire-api/" src="icons/Github.svg">Github</IconLink>
     <IconLink href="https://cmgt.hr.nl/projecten/claire/" src="icons/Website.svg">Web</IconLink>
     <p slot="description">
-    <strong>Role</strong>: Back-end Developer
+    <strong>Rol</strong>: Back-end Ontwikkelaar
     <br>
-    <strong>Technologies</strong>: NodeJS
+    <strong>Technologieën</strong>: NodeJS
     <br>
     <br>
-    <em>Claire is an air quality sensor that gives personalized advice based on your indoor climate. I developed it's API, which accomodates both an IoT device and a mobile app.</em>
+    <em>Claire is een sensor die op basis van de luchtkwaliteit binnen jouw huis gepersonaliseerd advies geeft. I heb de API gebouwd, die met zowel een IoT apparaat als een mobiele app communiceert.</em>
     </p>
   </ProjectCard>
   <ProjectCard image="charon_login.png" alt="Login prompt asking for a code and a password in Dutch. Text saying Charon Login." layout="invert">
     <h2>Charon Login</h2>
     <IconLink href="https://github.com/weareyipyip/charon_login/" src="icons/Github.svg">Github</IconLink>
     <p slot="description">
-    <strong>Role</strong>: Back-end Developer
+    <strong>Rol</strong>: Back-end Ontwikkelaar
     <br>
-    <strong>Technologies</strong>: Elixir
+    <strong>Technologieën</strong>: Elixir
     <br>
     <br>
-    <em>CharonLogin is an Elixir library for modular authentication procedures. Developers can define multi-factor authentication flows with a couple lines of configuration. Developed at YipYip.</em>
+    <em>CharonLogin is een Elixir library om modulair authenticatie op te zetten. Ontwikkelaars kunnen in een paar lijnen aan configuratie multi-factor authenticatie opzetten. Ik heb dit bij YipYip ontwikkeld.</em>
     </p>
   </ProjectCard>
   <ProjectCard image="missmatch.png" alt="Color wheel with a face and a bow. Text saying MissMatch.">
@@ -54,12 +54,12 @@
     <IconLink href="https://github.com/MattPermentier/MisMatch/" src="icons/Github.svg">Github</IconLink>
     <IconLink href="https://cmgt.hr.nl/projecten/missmatch/" src="icons/Website.svg">Showcase Site</IconLink>
     <p slot="description">
-    <strong>Role</strong>: Developer
+    <strong>Rol</strong>: Ontwikkelaar
     <br>
-    <strong>Technologies</strong>: PHP, WordPress
+    <strong>Technologieën</strong>: PHP, WordPress
     <br>
     <br>
-    <em>MissMatch is a WordPress plug-in to make your site more accessible. It looks at the colors used on your site and transforms them to increase the contrast, taking into account the WCAG.</em>
+    <em>missMatch is een WordPress plugin om sites toegankelijker te maken. Het kijkt naar de kleuren op een site en verandert ze om het contrast te verhogen in lijn met de WCAG.</em>
     </p>
   </ProjectCard>
   <ProjectCard image="catch.png" alt="3D rendered face with mouth open reaching for an apple." layout="invert">
@@ -67,12 +67,12 @@
     <IconLink href="https://github.com/UpperCi/catch/" src="icons/Github.svg">Github</IconLink>
     <IconLink href="https://upperci.github.io/catch/" src="icons/Website.svg">Live Demo</IconLink>
     <p slot="description">
-    <strong>Role</strong>: Solo Project
+    <strong>Rol</strong>: Solo Project
     <br>
-    <strong>Technologies</strong>: ML5.js, HTML Canvas
+    <strong>Technologieën</strong>: ML5.js, HTML Canvas
     <br>
     <br>
-    <em>A webgame to try out ML5.js. Using your webcam, it tracks facial movement and uses this as the controls.</em>
+    <em>Een webgame om ML5.js uit te proberen. Het gebruikt je webcam om de bewegingen die je gezicht maakt bij te houden, dit gebruik je om het spel te besturen.</em>
     </p>
   </ProjectCard>
 </div>
@@ -87,31 +87,31 @@
     <IconLink href="https://store.steampowered.com/app/2719710/Manor_Madness/" src="icons/Steam.svg">Steam</IconLink>
     <IconLink href="https://project.cmi.hr.nl/2023_2024/game_apricot/" src="icons/Website.svg">Apericot Studio</IconLink>
     <p slot="description">
-    <strong>Role</strong>: Game Designer
+    <strong>Rol</strong>: Game Designer
     <br>
-    <strong>Technologies</strong>: Unity 3D
+    <strong>Technologieën</strong>: Unity 3D
     <br>
     <br>
-    <em>Manor Madness is a 3D, third-person sandbox game. It focuses on fun derived from physical comedy. As a designer, I conducted weekly playtests and helped shape the gameplay mechanics.</em>
+    <em>Manor Madness is een 3D, third-person sanbox spel. Het richt zich op grappige situaties. Als designer heb ik wekelijks playtests afgenomen, en geholpen met het vormen van de gameplay.</em>
     </p>
   </ProjectCard>
   <ProjectCard image="gamejams.png" alt="Four game titles: Unplugged, Pursuit, Earth Apple and Wager." layout="invert">
     <h2>Game Jams</h2>
     <IconLink href="https://upperc.itch.io/" src="icons/Website.svg">Itch.io</IconLink>
     <p slot="description">
-    <strong>Role</strong>: Developer, Designer, Artist
+    <strong>Rol</strong>: Ontwikkelaar, Designer, Artiest
     <br>
-    <strong>Technologies</strong>: Godot, PICO-8
+    <strong>Technologieën</strong>: Godot, PICO-8
     <br>
     <br>
-    <em>Over the years I have participated in multiple game jams, forcing me to design and develop games in a short time-span, typically a weekend.</em>
+    <em>Over de loop van jaren heb ik aan meerdere game jams meegedaan, hierbij moet ik in korte tijd een game maken, meestal een weekend.</em>
     </p>
   </ProjectCard>
 </div>
 </div>
 
 <SiteObject>
-  <Subtitle>About Dani</Subtitle>
+  <Subtitle>Over Dani</Subtitle>
 <IconLink href="https://cmgt.hr.nl/projecten/claire/" src="icons/Github.svg" dark={true}>GitHub</IconLink>
 <IconLink href="https://cmgt.hr.nl/projecten/claire/" src="icons/LinkedIn.svg" dark={true}>LinkedIn</IconLink>
 <IconLink href="https://cmgt.hr.nl/projecten/claire/" src="icons/Website.svg" dark={true}>Curriculum Vitae</IconLink>
